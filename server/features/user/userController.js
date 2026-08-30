0f4249752e9fe19242e34ad03834aebfe662f052
Maya <MayaOfTheVoid@gmail.com>
2026-08-30 16:01:24 +0300

    feat: include a connection field in user indexing and search to indicate each users relation to current user

--- server/features/user/userController.js
+++ server/features/user/userController.js
@@ -46,11 +46,45 @@ const getUser = async(req,res, next)=>{
 const searchUsers = async(req, res, next)=>{
     //validation handler
     const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status(400).json({errors : errors.array()})
-    const {name} = matchedData(req);  
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()})
+    }
+    const {name,limit, cursor} = matchedData(req);  
+    let usersList;
     try{
-        const usersList = await service.findMatchingUsers(name);
-        return res.status(200).json({users: usersList})
+        console.log(name)
+        if(name === '' || name === undefined || name === null){
+            console.log('fetching user index')
+            usersList = await service.getAllUsers(req.user.id);
+        }else{
+            usersList = await service.findMatchingUsers(name, req.user.id,limit, cursor);            
+        }
+
+        if(usersList.chunk.length === 0) return res.status(404).json({message: 'User not found'})
+        
+        //sanitizing data to reflect each users connection status to current user
+        const sanitizedUsers= usersList.chunk.map(user=>{
+            const sentConnection = user.friendSent;
+            const recievedConnection = user.friendsRecieved;
+            let status = 'NONE';
+            sentConnection.length > 0? status = sentConnection.statuse : status;
+            recievedConnection.length > 0?status = recievedConnection.statuse : status
+             
+            const newUser={
+                id: user.id,
+                name: user.name,
+                photo: user.photo,
+                connection: status
+            }
+            return newUser
+        })
+        console.log(sanitizedUsers)
+        return res.status(200).json({
+            users: sanitizedUsers.chunk,
+            nextCursor: usersList.nextCursor !== null
+                ? usersList.nextCursor.id
+                : null
+        })
     }catch(err){
         next(err);
     }
--- server/features/user/userService.js
+++ server/features/user/userService.js
@@ -29,29 +29,143 @@ const updateMyData= async(id, data, photo = null)=>{
 const isUserPrivate = async(id)=>{
 
 }
-const findMatchingUsers= async(name)=>{
-    const users = await prisma.user.findMany({
-        where:{
-            name: {
-            startsWith: String(name)
-            }
-        },
-        select:{
-            id: true,
-            name: true,
-            photo: true
-        },
-        orderBy:{
-            name: "desc",
-        },
-    })
-    return users
+const findMatchingUsers= async(name, userId, limit = 15, cursor= null)=>{
+    //uuser search pagination
+    let rawChunk;
+    if(cursor){
+        rawChunk = await prisma.user.findMany({
+            where:{
+                name:{startsWith: String(name)}
+            },
+            orderBy:{
+                createdAt: 'desc'
+            },
+            select:{
+                id: true,
+                name: true,
+                photo: true,
+                friendSent:{
+                    where: {friendId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                } ,
+                friendsRecieved:{
+                    where: {userId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                }
+            },
+            take: Number(limit)+ 1,
+            cursor: {
+                id: Number(cursor),
+            },
+        })        
+    }else{
+        rawChunk = await prisma.user.findMany({
+            where:{
+                name:{startsWith: String(name)}
+            },
+            orderBy:{
+                createdAt: 'desc'
+            },
+            select:{
+                id: true,
+                name: true,
+                photo: true,
+                friendSent:{
+                    where:{friendId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                } ,
+                friendsRecieved:{
+                    where:{userId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                }
+            },            
+            take: Number(limit)+ 1,         
+        })  
+    }
+    const chunk = rawChunk.slice(0, -1)
+    const nextCursor = rawChunk[rawChunk.length - 1]
+    return {chunk, nextCursor};
+}
+const getAllUsers = async(userId, limit =15, cursor= null)=>{
+    //uuser index pagination
+    let rawChunk;
+    if(cursor){
+        rawChunk = await prisma.user.findMany({
+            orderBy:{
+                createdAt: 'desc'
+            },
+            select:{
+                id: true,
+                name: true,
+                photo: true,
+                friendSent:{
+                    where: {friendId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                } ,
+                friendsRecieved:{
+                    where: {userId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                }
+            },
+            take: Number(limit)+ 1,
+            cursor: {
+                id: Number(cursor),
+            },
+        })        
+    }else{
+        rawChunk = await prisma.user.findMany({
+            orderBy:{
+                createdAt: 'desc'
+            },
+            select:{
+                id: true,
+                name: true,
+                photo: true,
+                friendSent:{
+                    where:{friendId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                } ,
+                friendsRecieved:{
+                    where:{userId: Number(userId)},
+                    select:{
+                        status: true
+                    }
+                }
+            },            
+            take: Number(limit)+ 1,         
+        })  
+    }
+    //validate that prisma has enough records for pagination
+    const  hasEnoughRecords =  rawChunk.length > Number(limit);
+
+    const chunk = hasEnoughRecords
+        ? rawChunk.slice(0, -1)
+        : rawChunk
+    const nextCursor = hasEnoughRecords
+        ? rawChunk[rawChunk.length - 1]
+        : null;
+    return {chunk, nextCursor};
 }
 const service = {
     getUser,
     updateMyData,
     isUserPrivate,
     findMatchingUsers,
+    getAllUsers
 }
 export {
     service
