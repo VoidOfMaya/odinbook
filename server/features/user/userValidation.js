5ee1317e6305f2b5dae2f1b21624c0fef80189c9
Maya <MayaOfTheVoid@gmail.com>
2026-09-22 15:22:51 +0300

    fix: make user edit validation treat inputs as optional if no value provided and empty strings as no value provided

--- server/features/user/userValidation.js
+++ server/features/user/userValidation.js
@@ -1,11 +1,11 @@
 import { body,param,query } from 'express-validator';
 
 const userEdit = [
-    body('name').trim().isLength({max:25, min: 3}).withMessage('name must be 3 - 25 characters in length')
+    body('name').optional({ values: 'falsy' }).trim().isLength({max:25, min: 3}).withMessage('name must be 3 - 25 characters in length')
     .matches(/^[a-zA-Z ]+$/).withMessage('name can have letters and spaces'),
-    body('bio').trim().isLength({max:150}).withMessage('bio limit is 250 characters in length')
+    body('bio').optional({ values: 'falsy' }).trim().isLength({max:150}).withMessage('bio limit is 250 characters in length')
     .withMessage('bio can only be letters, spaces and numbers'),
-    body('photo').trim()
+    body('photo').optional({ values: 'falsy' }).trim()
 ]
 const userId =[
     param('id').trim().notEmpty().withMessage('no user id provided')
